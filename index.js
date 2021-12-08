bbead74f7a4047cde4e4fe777fc92fdbe13764a6
Atef Ben Ali <atef.bettaib@gmail.com>
2021-12-08 12:01:18 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import figlet from "figlet";
+import shell from "shelljs";
+
+function init() {
+    console.log(
+        chalk.green(
+            figlet.textSync("Node JS CLI", {
+                font: "Ghost",
+                horizontalLayout: "default",
+                verticalLayout: "default"
+            })
+        )
+    );
+}
+
+const askQuestions = () => {
+    const questions = [
+        {
+            name: "BRANCHES",
+            type: "input",
+            message: "What is the name of branches to rebase?"
+        },
+        {
+            type: "input",
+            name: "MAIN_BRANCH",
+            message: "What is the main branch?",
+        }
+    ];
+
+    return inquirer.prompt(questions);
+};
+
+async function run() {
+    // ask questions
+    const answers = await askQuestions();
+    const { BRANCHES, MAIN_BRANCH } = answers;
+    console.log(BRANCHES.split(" "), MAIN_BRANCH)
+
+
+
+    BRANCHES.split(" ").forEach(branch => {
+        console.log(`git switch ${branch}`)
+
+        if (!shell.which('git')) {
+            shell.echo('Sorry, this script requires git to be installed on your machine.');
+            shell.exit(1);
+        } else {
+            shell.exec(`git switch ${branch}`)
+        }
+
+        console.log(`git pull --rebase origin ${MAIN_BRANCH}`)
+    });
+
+    console.log(
+        chalk.white.bgGreen.bold(`Done!`)
+    );
+};
+
+run();
