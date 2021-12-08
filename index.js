7c7320f02233c05d6de3c6f55cedad98dfa38071
Atef Ben Ali <atef.bettaib@gmail.com>
2021-12-08 13:51:58 +0100

    chore: clean-up

--- index.js
+++ index.js
@@ -2,20 +2,7 @@
 
 import inquirer from "inquirer";
 import chalk from "chalk";
-import figlet from "figlet";
 import shell from "shelljs";
-
-function init() {
-    console.log(
-        chalk.green(
-            figlet.textSync("Node JS CLI", {
-                font: "Ghost",
-                horizontalLayout: "default",
-                verticalLayout: "default"
-            })
-        )
-    );
-}
 
 const askQuestions = () => {
     const questions = [
@@ -35,16 +22,10 @@ const askQuestions = () => {
 };
 
 async function run() {
-    // ask questions
     const answers = await askQuestions();
     const { BRANCHES, MAIN_BRANCH } = answers;
-    console.log(BRANCHES.split(" "), MAIN_BRANCH)
-
-
 
     BRANCHES.split(" ").forEach(branch => {
-        console.log(`git switch ${branch}`)
-
         if (!shell.which('git')) {
             shell.echo('Sorry, this script requires git to be installed on your machine.');
             shell.exit(1);
