9e08e3619b2996125fd4c5a4ceeee2b966045f45
Atef Ben Ali <atef.bettaib@gmail.com>
2021-12-09 12:27:53 +0100

    style: format

--- src/index.ts
+++ src/index.ts
@@ -3,53 +3,54 @@ import chalk from "chalk";
 import shell from "shelljs";
 
 function askQuestions() {
-  const questions = [
-    {
-      name: "BRANCHES",
-      type: "input",
-      message: "What is the name of branches to rebase?",
-    },
-    {
-      type: "input",
-      name: "MAIN_BRANCH",
-      message: "What is the main branch?",
-    },
-  ];
+    const questions = [
+        {
+            name: "BRANCHES",
+            type: "input",
+            message: "What is the name of branches to rebase?",
+        },
+        {
+            type: "input",
+            name: "MAIN_BRANCH",
+            message: "What is the main branch?",
+        },
+    ];
 
-  return inquirer.prompt(questions);
+    return inquirer.prompt(questions);
 }
 
 function askForForcePush() {
-  return inquirer.prompt({
-    type: "list",
-    name: "FORCE_PUSH",
-    message: "You wanna force pushing? (Be careful! This action is dangerous!)",
-    choices: ["Yes", "No"],
-  });
+    return inquirer.prompt({
+        type: "list",
+        name: "FORCE_PUSH",
+        message:
+            "You wanna force pushing? (Be careful! This action is dangerous!)",
+        choices: ["Yes", "No"],
+    });
 }
 
 async function run() {
-  const { BRANCHES, MAIN_BRANCH } = await askQuestions();
+    const { BRANCHES, MAIN_BRANCH } = (await askQuestions()) as any;
 
-  if (!shell.which("git")) {
-    shell.echo(
-      "Sorry, this script requires git to be installed on your machine."
-    );
-    shell.exit(1);
-  }
-
-  BRANCHES.split(" ").forEach((branch: string) => {
-    shell.exec(`git switch ${branch}`);
-    shell.exec(`git pull --rebase origin ${MAIN_BRANCH}`);
-
-    const { FORCE_PUSH } = askForForcePush() as any;
-
-    if (FORCE_PUSH === "Yes") {
-      shell.exec("git push --force");
+    if (!shell.which("git")) {
+        shell.echo(
+            "Sorry, this script requires git to be installed on your machine."
+        );
+        shell.exit(1);
     }
-  });
 
-  console.log(chalk.white.bgGreen.bold(`Done!`));
+    BRANCHES.split(" ").forEach((branch: string) => {
+        shell.exec(`git switch ${branch}`);
+        shell.exec(`git pull --rebase origin ${MAIN_BRANCH}`);
+
+        const { FORCE_PUSH } = askForForcePush() as any;
+
+        if (FORCE_PUSH === "Yes") {
+            shell.exec("git push --force");
+        }
+    });
+
+    console.log(chalk.white.bgGreen.bold(`Done!`));
 }
 
 run();
